f7fd51e5b05f5e65f3d0c4603c13e849e7080242
RAJI-Zakaria <zakariaraji.me@gmail.com>
2024-04-07 11:43:39 +0200

    adding theme provider and switcher

---
+++ app/providers.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import * as React from "react";
+import { NextUIProvider } from "@nextui-org/system";
+import { useRouter } from 'next/navigation'
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ThemeProviderProps } from "next-themes/dist/types";
+
+export interface ProvidersProps {
+	children: React.ReactNode;
+	themeProps?: ThemeProviderProps & { children: React.ReactNode };
+}
+
+ function Providers({ children }: ProvidersProps) {
+	const themeProps = {
+		attribute: "class", defaultTheme: "light", children,
+	}
+  const router = useRouter();
+
+	return (
+		<NextUIProvider navigate={router.push}>
+			<NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+		</NextUIProvider>
+	);
+}
+export default Providers;
