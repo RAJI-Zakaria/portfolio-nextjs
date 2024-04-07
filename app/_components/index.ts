2544836e8872041ee92cbd88b0f897cfac4d7c0f
RAJI-Zakaria <zakariaraji.me@gmail.com>
2024-04-07 11:45:15 +0200

    setting up layout with stars effect

---
+++ app/_components/index.ts
@@ -0,0 +1,5 @@
+export {default as NavBar} from './common/NavBar';
+export {default as ThemeSwitch} from './common/ThemeSwitch';
+export {default as Icons} from './common/Icons';
+export {default as Logo} from './common/Logo';
+export {default as StarField} from './common/StarField';
