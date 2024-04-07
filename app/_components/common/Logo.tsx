d938dd58d4386fbde38825980c690b03718128ef
RAJI-Zakaria <zakariaraji.me@gmail.com>
2024-04-07 11:44:15 +0200

    setting up the navbar and logo icons

---
+++ app/_components/common/Logo.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+const Logo = () => (
+    <svg data-name="Logo zkr" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 114.73 130.31" width='35'  className="fill-black dark:fill-slate-200">
+     
+      <g data-name="Zkr logo">
+        <g>
+          <path className="cls-1" d="M58.75,66.32h55.74v-13.02h-36.05L114.53,9.13V0H.04v13.02h36.05L0,57.19v9.13h58.75ZM22.7,53.3L55.77,12.83v.19h36.05l-33.07,40.48v-.19H22.7Z"/>
+          <polygon className="cls-1" points="114.73 88.22 114.73 72.19 0 72.19 0 88.22 31.09 88.22 45.1 94.63 0 111.14 0 130.31 63.53 106.38 114.73 129.89 114.73 111.01 66.22 89.09 66.22 88.22 114.73 88.22"/>
+        </g>
+      </g>
+    </svg>
+);
+
+
+export default Logo;
